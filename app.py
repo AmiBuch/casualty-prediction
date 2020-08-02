dd50008b58f35b5945eb956803f53b2844180b4c
AmiBuch <64002199+AmiBuch@users.noreply.github.com>
2020-08-02 21:49:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn import linear_model
-
+import xgboost as xgb
 import flask
 #reading my csv file from the url
 data = pd.read_csv("https://raw.githubusercontent.com/AmiBuch/HurricaneKat-python-ml-data/master/HurricaneDat.csv")
 #specifying the data on which the casualties/deaths are predicted
 x = data[['Hlat', 'Hlong', 'MaxSusWinds', 'Clat', 'Clong', 'Pop', 'Area']]
 y = data['Death']
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.1, random_state = 1)
 #training my model
-reg = linear_model.LinearRegression()
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.1, random_state = 1)
+clf = xgb.sklearn.XGBClassifier(nthread = -1, seed = 1)
+clf.fit(x_train, y_train)
 
-reg.fit(x_train, y_train)
 app = flask.Flask(__name__, template_folder = 'templates')
 @app.route('/', methods=['GET', 'POST'])
 def main():
@@ -27,7 +27,7 @@ def main():
         Pop = flask.request.form['Pop']
         Area = flask.request.form['Area']
         input_variables = pd.DataFrame([['Hlat', 'Hlong', 'MaxSusWinds', 'Clat', 'Clong', 'Pop', 'Area']], columns = [['Hlat', 'Hlong', 'MaxSusWinds', 'Clat', 'Clong', 'Pop', 'Area']], dtype=float)
-        prediction = reg.predict(input_variables)
+        prediction = clf.predict(input_variables)
         return flask.render_template('main.html', original_input = {'Hurricane Latitude':Hlat, 'Hurricane Longitude':Hlong, 'Maximum Sustained Winds (in knots)':MaxSusWinds, 'Country Latitude':Clat, 'Country Longitude':Clong, 'Country Population':Pop, 'Country Area (in square km)':Area}, result = prediction,)
 
 if __name__ == '__main__':

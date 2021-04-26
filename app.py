30156859c2500963dad8ff660bb270c7595d54d7
Ami Buch <64002199+AmiBuch@users.noreply.github.com>
2021-04-26 20:04:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,84 +1,43 @@
-
-
-import nltk
-from nltk.corpus import stopwords 
-from nltk.tokenize import word_tokenize
-import pytesseract
-import cv2
-from skimage import io
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
-
-import string 
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn import linear_model
+from sklearn import ensemble
 import flask
-def hand_core(im):
-	text = pytesseract.image_to_string(im)
-	return text
+#reading my csv file from the url
+data = pd.read_csv("https://raw.githubusercontent.com/AmiBuch/HurricaneKat-python-ml-data/master/HurricaneDat.csv")
+#specifying the data on which the casualties/deaths are predicted
+x = data[['Hlat', 'Hlong', 'MaxSusWinds', 'Clat', 'Clong', 'Pop', 'Area']]
+y = data['Death']
+#training my model
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.1, random_state = 1)
+reg = linear_model.LinearRegression()
+clf = ensemble.GradientBoostingRegressor(n_estimators = 400, max_depth = 5, min_samples_split = 2, learning_rate = 0.1, loss = 'ls')
+clf.fit(x_train, y_train)
 
-def get_grayscale(image):
-	return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-def remove_noise(image):
-	return cv2.medianBlur(image, 5)
-
-def thresholding(image):
-	return cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
-def word_tokenizer(sents):
-	tokenized_glop = nltk.tokenize.word_tokenize(sents)
-	return tokenized_glop    
-stopwords_en = stopwords.words("english")
 app = flask.Flask(__name__, template_folder = 'templates')
 @app.route('/', methods=['GET', 'POST'])
 def main():
-	if flask.request.method == 'GET':
-		return(flask.render_template('main.html'))
-	if flask.request.method == 'POST':
-		answer = str(flask.request.form['cor_ans'])
-        	marks = int(flask.request.form['marks_me'])
-        	Image_dumb = flask.request.form['dumb']
-        	img = io.imread(Image_dumb)
-        	img = get_grayscale(img)
-        	img = thresholding(img)
-        	img = remove_noise(img)
-        	yo_ans = hand_core(img)
-        	plog = word_tokenizer(yo_ans)
-        	plog_real = word_tokenizer(answer)
-        	raw_deal = [w.lower() for w in plog if w not in stopwords_en]
-        	real_deal = [w.lower() for w in plog if w not in stopwords_en]
-        	deal1 = ""
-        	for i in raw_deal:
-			deal1 += str(i)
-			deal1 += " "          
-        	deal2 = ""
-        	for i in real_deal:
-			deal2 += str(i)
-			deal2 += " "       
-          
-        	documents = (deal1, deal2)
-        	tfidf_vectorizer = TfidfVectorizer()
-        	tfidf_matrix = tfidf_vectorizer.fit_transform(documents)
-        	my_array = cosine_similarity(tfidf_matrix[0:1], tfidf_matrix)
-        	relief = my_array[:][-1][-1]
-        	if 0.15 < relief < 0.25:
-			prediction = str("marks scored : 2")
-          
-		elif 0 < relief < 0.15:
-			prediction = str("marks scored : 1")
-	
-        	else:
-			prediction = str("marks scored : 3")
-          
-       
-        	return flask.render_template('main.html', result = prediction,)
+    if flask.request.method == 'GET':
+        return(flask.render_template('main.html'))
+    if flask.request.method == 'POST':
+        Hlat = flask.request.form['Hlat']
+        Hlong = flask.request.form['Hlong']
+        MaxSusWinds = flask.request.form['MaxSusWinds']
+        Clat = flask.request.form['Clat']
+        Clong = flask.request.form['Clong']
+        Pop = flask.request.form['Pop']
+        Area = flask.request.form['Area']
+        Image_dumb = flask.request.form['dumb']
+        input_variables = pd.DataFrame([[Hlat, Hlong, MaxSusWinds, Clat, Clong, Pop, Area]], columns = ['Hlat', 'Hlong', 'MaxSusWinds', 'Clat', 'Clong', 'Pop', 'Area'], dtype=float)
+        prediction = type(Image_dumb)
+        #clf.predict(input_variables)
         
-	
-    
-    
-        
-        
+        if prediction < 0:
+            prediction = 0
+        return flask.render_template('main.html', original_input = {'Hurricane Latitude':Hlat, 'Hurricane Longitude':Hlong, 'Maximum Sustained Winds (in knots)':MaxSusWinds, 'Country Latitude':Clat, 'Country Longitude':Clong, 'Country Population':Pop, 'Country Area (in square km)':Area}, result = prediction,)
 
 if __name__ == '__main__':
     app.run()
-
-
-
+#model_pickle = "C:\Desktop\webapp\model\prediction_model.pkl"
+#with open(model_pickle, 'wb') as file:
+ #   pickle.dump(clf, file)
